54440115d73f050aede5f179025103815483fe24
MaTtiaa01 <mattia2001@live.it>
2022-10-05 15:36:44 +0200

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
-
+alert("password generator");
 //istruzioni
 
 //Chiedi all’utente il suo nome,
+const user_name = prompt("Qual'è il tuo nome?");
+
 //poi chiedi il suo cognome,
+const user_surnume = prompt("Qual'è il tuo cognome?");
+
 //poi chiedi il suo colore preferito
+const favourite_color = prompt("Qual'è il tuo colore preferito?");
 //Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+document.getElementById("password").innerHTML = user_name + user_surnume + favourite_color;
