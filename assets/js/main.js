531315f2b05d120bac2188c68604c3ea41d61bda
MaTtiaa01 <mattia2001@live.it>
2022-10-05 16:11:39 +0200

    change const name

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ alert("password generator");
 //istruzioni
 
 //Chiedi all’utente il suo nome,
-const user_name = prompt("Qual'è il tuo nome?");
+const userName = prompt("Qual'è il tuo nome?");
 
 //poi chiedi il suo cognome,
-const user_surnume = prompt("Qual'è il tuo cognome?");
+const userSurnume = prompt("Qual'è il tuo cognome?");
 
 //poi chiedi il suo colore preferito
-const favourite_color = prompt("Qual'è il tuo colore preferito?");
+const favouriteColor = prompt("Qual'è il tuo colore preferito?");
 //Infine scrivi sulla pagina nomecognomecolorepreferito22
-const current_year = 2022
+const currentYear = 2022
 
-document.getElementById("password").innerHTML = user_name + user_surnume + favourite_color + current_year ;
+document.getElementById("password").innerHTML = userName + userSurnume + favouriteColor + currentYear ;
